c235ece315a7d9444d037a2014ef141c619baf40
sruthimol sasi <sruthimolsasi1994@gmail.com>
2022-09-24 10:16:04 +0530

    event binding using template rendering methord

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,34 +24,61 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  login(){
-     var acnum=this.acno
-     var pswd= this.pswd
-     let userDetails=this.userDetails
-     if (acnum in userDetails)
-     {
-       if(pswd==userDetails[acnum]['password']){
-         alert('login success')
-        }
-        else {
-        alert('incorrect password')
-        }
-    }
-    else{
-      alert('user not exist or incorrect password')
-    }
+                          // in the case of $
+  // login(){
+  //    var acnum=this.acno
+  //    var pswd= this.pswd
+  //    let userDetails=this.userDetails
+  //    if (acnum in userDetails)
+  //    {
+  //      if(pswd==userDetails[acnum]['password']){
+  //        alert('login success')
+  //       }
+  //       else {
+  //       alert('incorrect password')
+  //       }
+  //   }
+  //   else{
+  //     alert('user not exist or incorrect password')
+  //   }
     
-  }
-  acnochange(event:any)
-  {
-     this.acno=event.target.value
-     console.log(this.acno);
-  }
-  pswdchange(event:any)
-  {
-    this.pswd=event.target.value
-    console.log(this.pswd)
-  }
+  // }
+
+
+  // acnochange(event:any)
+  // {
+  //    this.acno=event.target.value
+  //    console.log(this.acno);
+  // }
+  // pswdchange(event:any)
+  // {
+  //   this.pswd=event.target.value
+  //   console.log(this.pswd)
+  // }
+                                      // in the case of #
+   //-----------------------------------------------
+  login( a:any,b:any){
+    // console.log(a.value)
+    // console.log(b.value)
+
+    var acnum=a.value
+    var pswd=b.value
+
+    let userDetails=this.userDetails
+    if (acnum in userDetails)
+    {
+      if(pswd==userDetails[acnum]['password']){
+        alert('login success')
+       }
+       else {
+       alert('incorrect password')
+       }
+   }
+   else{
+     alert('user not exist or incorrect password')
+   }
+   
+ }
+  
 
 }
